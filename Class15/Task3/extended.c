4d4d6a0d5f3da5529534a7f5f5f234336627e449
VirtualJabberWock <115100398+VirtualJabberWock@users.noreply.github.com>
2022-12-09 00:33:20 +0500

    BUG FIX
    fix

--- Class15/Task3/extended.c
+++ Class15/Task3/extended.c
@@ -1,43 +1,6 @@
 #include "extended.h"
 
-typedef struct tagPiece {
-	char x;
-	char y;
-} Piece;
 
-int check(Piece f[8]);
-
-int extended() {
-	Piece input[8] = { 0 };
-	for (int i = 0; i < 8; i++) {
-		int x = 0; int y = 0;
-		printf("(x, y)> ");
-		if (!scanf_s("%d %d", &x, &y)) panic("Invalid input");
-		if (x < 0 || x >= 8 || y < 0 || y >= 8) panic("Invalid coords");
-		input[i].x = x;
-		input[i].y = y;
-	}
-	int a = check(input);
-	if (a) printf("Pair no found!");
-	else printf("There is linked pair!");
-	return 0;
-}
-
-
-
-int check(Piece f[8])
-{
-	for (int factor = 1; factor < 5; factor++) {
-		int j = 8 - factor;
-		for (int i = 0; i != 8 - factor; (i + factor) % 8) {
-			int a =
-				(f[i].x == f[j].x) || (f[i].y == f[j].y) || (abs(f[i].x - f[j].x) == abs(f[i].y - f[j].y));
-			if (a) return 0;
-			j = i;
-		}
-	}
-	return 1; // 4 * 8 operations
-}
 
 //char b0 = 0;
 //char b1 = 1;
--- Class15/Task3/main.c
+++ Class15/Task3/main.c
@@ -3,7 +3,35 @@
 #include "../../aquaUtils/utils/DebugUtils.h"
 #include "extended.h"
 
+
+typedef struct tagPiece {
+	int x;
+	int y;
+} Piece;
+
 int main() {
-	return extended();
+	Piece f[8] = { 0 };
+	for (int i = 0; i < 8; i++) {
+		printf("(x, y)> ");
+		if (!scanf_s("%d %d", &f[i].x, &f[i].y)) panic("Invalid input");
+		if (f[i].x < 0 || f[i].x >= 8 || f[i].y < 0 || f[i].y >= 8) panic("Invalid coords");
+	}
+	for (int factor = 1; factor < 5; factor++) {
+		int j = 8 - factor;
+		int i = 0;
+		while (i != 8 - factor) {
+			if (j == i) continue;
+			int a = (f[i].x == f[j].x);
+			a = a || (f[i].y == f[j].y);
+			a = a || (abs(f[i].x - f[j].x) == abs(f[i].y - f[j].y));
+			if (a) {
+				return printf("There is linked pair!");
+			}
+			j = i;
+			i = (i + factor) % 8;
+		}
+	}
+	printf("There is no linked pairs");
+	return 0;
 }
 
